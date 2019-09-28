9b974965b6fb1edbf3971d334b41beb9954e1c12
Alex <wolfplayeral@yahoo.co.uk>
2019-09-28 23:53:52 +0100

    Added multiplications for uneven matrices (i.e. 2x3 by 3x2)

--- Clove/modules/maths/Matrix.hpp
+++ Clove/modules/maths/Matrix.hpp
@@ -17,18 +17,24 @@ private:
 public:
 	
 
-	auto& operator[](std::uint32_t index){
+	auto& operator[](std::uint32_t index){ //TODO: add const []
 		return mat[index];
 	}
 };
 
-template<std::uint32_t R, std::uint32_t C, typename T>
-constexpr Matrix<R, C, T> operator*(const Matrix<R, C, T>& lhs, const Matrix<R, C, T>& rhs){
-	Matrix<R, C, T> result;
+template<std::uint32_t a, std::uint32_t b>
+struct min{
+	constexpr static std::uint32_t value = a < b ? a : b;
+};
 
-	for(std::size_t row = 0; row < R; ++row){
-		for(std::size_t col = 0; col < C; ++col){
-			for(std::size_t i = 0, std::size_t j = 0; i < C && j < R; ++i, ++j){
+template<std::uint32_t aR, std::uint32_t aC, std::uint32_t bR, std::uint32_t bC, typename T>
+constexpr auto operator*(Matrix<aR, aC, T>& lhs, Matrix<bR, bC, T>& rhs){
+	constexpr auto minLen = min<aR, aC>::value;
+	Matrix<minLen, minLen, T> result;
+
+	for(std::size_t row = 0; row < minLen; ++row){
+		for(std::size_t col = 0; col < minLen; ++col){
+			for(std::size_t i = 0,  j = 0; i < aC && j < bR; ++i, ++j){
 				result[row][col] += lhs[row][i] * rhs[j][col];
 			}
 		}
@@ -36,3 +42,6 @@ constexpr Matrix<R, C, T> operator*(const Matrix<R, C, T>& lhs, const Matrix<R, 
 
 	return result;
 }
+
+
+//TODO: I think the row / colum needs adjusting because for a 2x3 it's row[
