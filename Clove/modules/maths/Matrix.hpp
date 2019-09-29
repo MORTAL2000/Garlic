d9e8b6429af53686a725d2a822d508ecd848db35
Alex <wolfplayeral@yahoo.co.uk>
2019-09-29 11:48:36 +0100

    Matrix multiplication will now work properly if outer row are different

--- Clove/modules/maths/Matrix.hpp
+++ Clove/modules/maths/Matrix.hpp
@@ -31,23 +31,25 @@ public:
 	}
 };
 
+
+//TODO: Put in some sort of global header
 template<std::uint32_t a, std::uint32_t b>
 struct sameValue{
-	static constexpr bool value = a == b;
+	static constexpr bool value = (a == b);
 };
 
 template<std::uint32_t aR, std::uint32_t aC, std::uint32_t bR, std::uint32_t bC, typename T,
-	std::enable_if_t<sameValue<aC, bR>::value, int> = 0, std::enable_if_t<sameValue<aR, bC>::value, int> = 0>
+	std::enable_if_t<sameValue<aC, bR>::value, int> = 0>
 constexpr auto operator*(const Matrix<aR, aC, T>& lhs, const Matrix<bR, bC, T>& rhs){
-	constexpr auto minLen = std::min(aR, aC);
-	Matrix<minLen, minLen, T> result;
+	Matrix<aR, bC, T> result;
 
-	for(std::size_t row = 0; row < minLen; ++row){
-		for(std::size_t col = 0; col < minLen; ++col){
+	for(std::size_t row = 0; row < aR; ++row){
+		for(std::size_t col = 0; col < bC; ++col){
 			for(std::size_t i = 0, j = 0; i < aC && j < bR; ++i, ++j){
 				result[row][col] += lhs[row][i] * rhs[j][col];
 			}
 		}
 	}
+
 	return result;
 }
