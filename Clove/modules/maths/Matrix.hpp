19ed53028f9ff488155bd925233cdfe6687da909
Alex <wolfplayeral@yahoo.co.uk>
2019-10-06 09:19:11 +0100

    Fixed the missing return matrix definition

--- Clove/modules/maths/Matrix.hpp
+++ Clove/modules/maths/Matrix.hpp
@@ -44,6 +44,8 @@ template<std::uint32_t aR, std::uint32_t aC, std::uint32_t bR, std::uint32_t bC,
 constexpr Matrix<aR, bC, T> operator*(const Matrix<aR, aC, T>& lhs, const Matrix<bR, bC, T>& rhs){
 	static_assert(sameValue<aC, bR>::value, "Inner values must be the same");
 
+	Matrix<aR, bC, T> result;
+
 	for(std::size_t row = 0; row < aR; ++row){
 		for(std::size_t col = 0; col < bC; ++col){
 			for(std::size_t i = 0, j = 0; i < aC && j < bR; ++i, ++j){
