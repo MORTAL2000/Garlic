45ca6d43646f00f773d0001e740ca475dce474cb
Alex <wolfplayeral@yahoo.co.uk>
2019-10-17 13:23:37 +0100

    Wrapped Matrix and Vector in the correct namespaces

--- Clove/modules/maths/Matrix.hpp
+++ Clove/modules/maths/Matrix.hpp
@@ -4,48 +4,49 @@
 #include <array>
 #include <type_traits>
 
-//TODO: Wrap in namespace
 //TODO: inl
 
-template<std::uint32_t R, std::uint32_t C, typename T>
-struct Matrix{
-	static_assert(std::is_arithmetic_v<T>, "Type is not arithmetic!");
+namespace clv::mth{
+	template<std::uint32_t R, std::uint32_t C, typename T>
+	struct Matrix{
+		static_assert(std::is_arithmetic_v<T>, "Type is not arithmetic!");
 
-	//VARIABLES
-private:
-	std::array<std::array<T, C>, R> mat;
+		//VARIABLES
+	private:
+		std::array<std::array<T, C>, R> mat;
 
-	//FUNCTIONS
-public:
-	Matrix(){
-		std::for_each(mat.begin(), mat.end(), [](auto& innerArray){
-			std::for_each(innerArray.begin(), innerArray.end(), [](T& value){
-				value = 0;
+		//FUNCTIONS
+	public:
+		Matrix(){
+			std::for_each(mat.begin(), mat.end(), [](auto& innerArray){
+				std::for_each(innerArray.begin(), innerArray.end(), [](T& value){
+					value = 0;
+				});
 			});
-		});
-	}
+		}
 
-	auto& operator[](std::uint32_t index){
-		return mat[index];
-	}
-	const auto& operator[](std::uint32_t index) const{
-		return mat[index];
-	}
-};
+		auto& operator[](std::uint32_t index){
+			return mat[index];
+		}
+		const auto& operator[](std::uint32_t index) const{
+			return mat[index];
+		}
+	};
 
-template<std::uint32_t aR, std::uint32_t aC, std::uint32_t bR, std::uint32_t bC, typename T>
-constexpr Matrix<aR, bC, T> operator*(const Matrix<aR, aC, T>& lhs, const Matrix<bR, bC, T>& rhs){
-	static_assert(aC == bR, "Inner values must be the same");
+	template<std::uint32_t aR, std::uint32_t aC, std::uint32_t bR, std::uint32_t bC, typename T>
+	constexpr Matrix<aR, bC, T> operator*(const Matrix<aR, aC, T>& lhs, const Matrix<bR, bC, T>& rhs){
+		static_assert(aC == bR, "Inner values must be the same");
 
-	Matrix<aR, bC, T> result;
+		Matrix<aR, bC, T> result;
 
-	for(std::size_t row = 0; row < aR; ++row){
-		for(std::size_t col = 0; col < bC; ++col){
-			for(std::size_t i = 0, j = 0; i < aC && j < bR; ++i, ++j){
-				result[row][col] += lhs[row][i] * rhs[j][col];
+		for(std::size_t row = 0; row < aR; ++row){
+			for(std::size_t col = 0; col < bC; ++col){
+				for(std::size_t i = 0, j = 0; i < aC && j < bR; ++i, ++j){
+					result[row][col] += lhs[row][i] * rhs[j][col];
+				}
 			}
 		}
-	}
 
-	return result;
+		return result;
+	}
 }
--- Clove/modules/maths/Vector.hpp
+++ Clove/modules/maths/Vector.hpp
@@ -4,60 +4,60 @@
 #include <array>
 #include <type_traits>
 
-/*
-TODO: enable if? don't think I can default it like this
-*/
+//TODO: inl
 
-template<std::uint32_t L, typename T>
-struct Vector{ };
+namespace clv::mth{
+	template<std::uint32_t L, typename T>
+	struct Vector{ };
 
-template<typename T>
-struct Vector<2, T>{
-	//VARIABLES
-public:
-	union{ T x; T r; T u; };
-	union{ T y; T g; T v; };
+	template<typename T>
+	struct Vector<2, T>{
+		//VARIABLES
+	public:
+		union{ T x; T r; T u; };
+		union{ T y; T g; T v; };
 
-	//FUNCTIONS
-public:
-	Vector(){
-		x = 0;
-		y = 0;
-	}
-};
+		//FUNCTIONS
+	public:
+		Vector(){
+			x = 0;
+			y = 0;
+		}
+	};
 
-template<typename T>
-struct Vector<3, T>{
-	//VARIABLES
-public:
-	union{ T x; T r; };
-	union{ T y; T g; };
-	union{ T z; T b; };
+	template<typename T>
+	struct Vector<3, T>{
+		//VARIABLES
+	public:
+		union{ T x; T r; };
+		union{ T y; T g; };
+		union{ T z; T b; };
 
-	//FUNCTIONS
-public:
-	Vector(){
-		x = 0;
-		y = 0;
-		z = 0;
-	}
-};
+		//FUNCTIONS
+	public:
+		Vector(){
+			x = 0;
+			y = 0;
+			z = 0;
+		}
+	};
 
-template<typename T>
-struct Vector<4, T>{
-	//VARIABLES
-public:
-	union{ T x; T r; };
-	union{ T y; T g; };
-	union{ T z; T b; };
-	union{ T w; T a; };
+	template<typename T>
+	struct Vector<4, T>{
+		//VARIABLES
+	public:
+		union{ T x; T r; };
+		union{ T y; T g; };
+		union{ T z; T b; };
+		union{ T w; T a; };
 
-	//FUNCTIONS
-public:
-	Vector(){
-		x = 0;
-		y = 0;
-		z = 0;
-		w = 0;
-	}
-};
+		//FUNCTIONS
+	public:
+		Vector(){
+			x = 0;
+			y = 0;
+			z = 0;
+			w = 0;
+		}
+	};
+}
