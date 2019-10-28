74a7b128e79210cd14dca8a1f1870d27aef3c121
Alex <wolfplayeral@yahoo.co.uk>
2019-10-28 13:27:51 +0000

    Added function definitions for the vector math

--- Clove/modules/maths/VectorMath.hpp
+++ Clove/modules/maths/VectorMath.hpp
@@ -1,8 +1,20 @@
-//TODO:
-//inline
-//dot
-//cross
+#include "Vector.hpp"
 
 namespace clv::mth{
+    template<length_type L, typename T>
+	Vector<L, T> normalise(const Vector<L, T>& vector);
 
+	template<typename T>
+	Vector<3, T> cross(const Vector<3, T>& vector1, const Vector<3, T>& vector2);
+
+	template<length_type L, typename T>
+	Vector<L, T> dot(const Vector<L, T>& vector1, const Vector<L, T>& vector2);
+
+	template<length_type L, typename T>
+	T distance(const Vector<L, T>& vector1, const Vector<L, T>& vector2);
+
+	template<length_type L, typename T>
+	T length(const Vector<L, T>& vector);
 }
+
+#include "VectorMath.inl"
---
+++ Clove/modules/maths/VectorMath.inl
@@ -0,0 +1,26 @@
+namespace clv::mth{
+	template<length_type L, typename T>
+	Vector<L, T> normalise(const Vector<L, T>& vector){
+		//TODO
+	}
+
+	template<typename T>
+	Vector<3, T> cross(const Vector<3, T>& vector1, const Vector<3, T>& vector2){
+		//TODO
+	}
+
+	template<length_type L, typename T>
+	Vector<L, T> dot(const Vector<L, T>& vector1, const Vector<L, T>& vector2){
+		//TODO
+	}
+
+	template<length_type L, typename T>
+	T distance(const Vector<L, T>& vector1, const Vector<L, T>& vector2){
+		//TODO
+	}
+
+	template<length_type L, typename T>
+	T length(const Vector<L, T>& vector){
+		//TODO
+	}
+}
