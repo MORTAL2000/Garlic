b46e2283b465e527472e51ce4bd74b88de92180b
Alex <wolfplayeral@yahoo.co.uk>
2019-10-28 13:57:11 +0000

    Added most recent tests

--- Clove/modules/maths/Test.cpp
+++ Clove/modules/maths/Test.cpp
@@ -2,6 +2,7 @@
 
 #include "Matrix.hpp"
 #include "Vector.hpp"
+#include "VectorMath.hpp"
 
 int main(){
     std::cout << "MATRICES" << std::endl;
@@ -55,19 +56,34 @@ int main(){
     std::cout << "a+b.x:" << ab[0] << " a+b.y:" << ab[1] << std::endl;
 
     clv::mth::Vector3f ca{34, -6, 200};
-    std::cout << "ca.x:" << ca[0] << " ca.y:" << ca[1] << " ca.z" << ca[2] << std::endl;
+    std::cout << "ca.x:" << ca[0] << " ca.y:" << ca[1] << " ca.z:" << ca[2] << std::endl;
 
     clv::mth::Vector3f cb{34, -6, 200};
-    std::cout << "cb.x:" << cb[0] << " cb.y:" << cb[1] << " cb.z" << cb[2] << std::endl;
+    std::cout << "cb.x:" << cb[0] << " cb.y:" << cb[1] << " cb.z:" << cb[2] << std::endl;
 
     auto cc = ca - cb;
-    std::cout << "cc.x:" << cc[0] << " cc.y:" << cc[1] << " cc.z" << cc[2] << std::endl;
+    std::cout << "cc.x:" << cc[0] << " cc.y:" << cc[1] << " cc.z:" << cc[2] << std::endl;
 
     clv::mth::Vector4f d{345.756f, 100.00002, 89, 1};
     std::cout << "d.x:" << d[0] << " d.y:" << d[1] << " d.z:" << d[2] << " d.w:" << d[3] << std::endl;
 
     auto d2 = d * 5.0f;
     std::cout << "d2.x:" << d2[0] << " d2.y:" << d2[1] << " d2.z:" << d2[2] << " d.w:" << d2[3] << std::endl;
+
+	std::cout << "length of a is: " << clv::mth::length(a) << std::endl;
+	std::cout << "a dot b is: " << clv::mth::dot(a, b) << std::endl;
+	std::cout << "length of ca is " << clv::mth::length(ca) << std::endl;
+	std::cout << "ca has been normalised" << std::endl;
+	auto can = normalise(ca);
+	std::cout << "length of caNorm is " << clv::mth::length(can) << std::endl;
+	std::cout << "can.x:" << can[0] << " can.y:" << can[1] << " can.z:" << can[2] << std::endl;
+
+	auto an = normalise(a);
+	auto bn = normalise(b);
+
+	std::cout << "aNorm dot bNorm is " << clv::mth::dot(an, bn) << std::endl;
+
+	std::cout << "a is " << clv::mth::distance(a, b) << " units away from b" << std::endl;
 
     return 0;
 }
